e90f550fe7fe141633c55df36078e165ad96d6c2
baghelshivam <baghelshivam11@gmail.com>
2022-08-02 02:21:33 +0530

    styling canvas

--- client/src/components/Fabric.jsx
+++ client/src/components/Fabric.jsx
@@ -1,41 +1,57 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { fabric } from 'fabric'
 
-import Header from "./Header";      //importing components
+import Header from "./HeaderFabric";      //importing components
 import Footer from "./Footer";
 
 const FabricJSCanvas = () => {				//canvas creating function
-	const canvasEl = useRef(null);			//created null reference for dom object (canvas)
+	// const canvasEl = useRef(null);			//created null reference for dom object (canvas)
 
-	useEffect(() => {						//tells to do something after rendring
-		const options = {					//options for canvas
-			backgroundColor: '#c5d5cb',
-			selectionColor: '#9fa8a3A9',	//added alpha in hex code last two digits
-			selectionBorderColor: 'black',
-			selectionLineWidth: 1,
-		};
-		var canvasConstant = window.innerHeight/100;
-		
-		const canvas = new fabric.Canvas(canvasEl.current, options);	/*new canvas element created by fabric 
-																		with reference priviously defined and give options*/
+	// useEffect(() => {						//tells to do something after rendring
+	// 	const options = {					//options for canvas
+	// 		backgroundColor: '#fff',
+	// 		selectionColor: '#9fa8a3A9',	//added alpha in hex code last two digits
+	// 		selectionBorderColor: 'black',
+	// 		selectionLineWidth: 1,
+	// 	};
+	// 	const canvas = new fabric.Canvas(canvasEl.current, options);	/*new canvas element created by fabric 
+	// 																	with reference priviously defined and give options*/
 
-		canvas.isDrawingMode = true;		//this property is for if we can draw with mouse pointer
-		canvas.setHeight(297*canvasConstant);//setting same size for all upper lower canvas can use for dynamic one too
-		canvas.setWidth(210*canvasConstant);
-		canvas.renderAll();
-	}										//useEffect ends
+	// 	// canvas.isDrawingMode = true;		//this property is for if we can draw with mouse pointer
+	// 	// canvas.setHeight(2.97 * canvas.getHeight()/1.5);//setting same size for all upper lower canvas can use for dynamic one too
+	// 	// canvas.setWidth(2.10 * canvas.getWidth()/1.5);
+	// 	canvas.renderAll();
+	// }										//useEffect ends
 
-		, []);
+	// 	, []);
 
-	return (								//returning div element conataining Fabric canvas
-		<div>
+	// return (								//returning div element conataining Fabric canvas
+	// 	<div>
+	// 		<Header />
+	// 				<canvas className="fabricCanvas" ref={canvasEl} />
+	// 		<Footer />
+	// 	</div>
+	// );
+	const [canvas, setCanvas] = useState();
+
+	useEffect(() => {
+		setCanvas(intitCanvas());
+	}, []);
+
+	const intitCanvas = () => (
+		new fabric.Canvas("canvas", {
+			height: 800,
+			width: 800,
+			backgroundColor: "white",
+			isDrawingMode: "true",
+		})
+	);
+
+	return (
+		<div className="fabric">
 			<Header />
-			<div className="fabricCanvas">
-				<div className="drawingPad">
-					<canvas ref={canvasEl} />
-				</div>
-			</div>
-			<Footer />
+			{/* <button onClick={() => freeHand(canvas)}>FreeDraw</button> */}
+			<canvas id="canvas" className="fabricCanvas" />
 		</div>
 	);
 };
---
+++ client/src/components/HeaderFabric.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+function Header() {
+    return (
+        <header className="header headerFabric flexItems">
+            <div className="flexItems">
+                <a href="/" className="logo">Wrikho</a>
+            </div>
+            <div className="flexItems">
+                <button>
+                <i className="bi-list"></i>
+                </button>
+                <button>
+                    <i className="bi-plus-lg"></i>
+                </button>
+                <button>
+                <i className="bi bi-search"></i>
+                </button>
+                </div>
+        </header>);
+}
+
+export default Header;
