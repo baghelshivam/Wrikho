11b489eae86730a697a0d4497042c8c4c39395be
baghelshivam <baghelshivam11@gmail.com>
2023-03-16 17:38:51 +0530

    Added loading component

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loading.css"
+function Loading() {
+  return (
+    <div className="loading">
+      <div className="loading-spinner" />
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
